0df362ac7e4d8e93eef56304b517314bb244abe8
Leytzher Muro <leytzher@gmail.com>
2018-06-20 19:30:17 +0800

    Get the (Brent) oil price

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
-const fetch = require('node-fetch');
+const request = require('request');
 
+// Link to oilprice.com API
 const priceUrl = 'https://oilprice.com/ajax/get_chart_prices/';
 
-//fetch(priceUrl)
-//    .then(res => res.text())
-//    .then(body => console.log(body));
+let price;
+request(priceUrl, (error, response, body) => {
+  result = JSON.parse(body);
+  wtiPrice = result[1][0];
+  wtiChange = result[1][2];
 
+  if (wtiChange > 0) {
+    trend = `this is an increase of ${wtiChange} percent`;
+  } else if (wtiChange < 0) {
+    trend = `this is a decrease of ${Math.abs(wtiChange)} percent`;
+  } else {
+    trend = 'there is no change in price';
+  }
 
-//fetch(priceUrl)
-//    .then(res => res.json())
-//    .then(json => console.log(json));
-
-
-
-const getPrices = () => {
-	return fetch(priceUrl)
-	.then(res=> res.json())
-	.then(json=> console.log(json))
-}
-
-
-
-var test = getPrices()
-console.log(test)
+  console.log(
+    `The WTI price is ${wtiPrice} USD, ${trend} within the last 15 minutes`
+  );
+});
